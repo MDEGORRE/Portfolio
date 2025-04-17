80415ac060364246442188ab888a4122c67800b4
MDEGORRE <degorremichael@gmail.com>
2025-04-17 22:01:31 +0200

    changed image src on projects component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
 import { onMounted } from 'vue'
 import { initDropdowns } from 'flowbite';
 
-onMounted(() => {
-  initDropdowns();
-})
-
 import Navbar from './components/Navbar.vue';
 import Timeline from './components/Timeline.vue';
 import ContactForm from './components/ContactForm.vue';
@@ -13,6 +9,10 @@ import Jumbotron from './components/Jumbotron.vue';
 import Competencies from './components/Competencies.vue';
 import Projects from './components/Projects.vue';
 import Footer from './components/Footer.vue';
+
+onMounted(() => {
+  initDropdowns();
+})
 
 </script>
 
