e096d24fd80c980facc408be2bbcc72ad1939306
MDEGORRE <degorremichael@gmail.com>
2025-04-17 15:30:42 +0200

    changed location of background image

---
+++ src/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        title: "O'Petits Oignons",
+        purpose: "Projet réalisé pour l'obtention du titre professionnel Développeur Web et Web Mobile.",
+        demoUrl: "https://www.opetitsoignons.io/",
+        image: "logo-oignon.webp",
+    },
+    {
+        title: "Final Fantasy XIV Market Helper",
+        purpose: "Projet personnel en Nuxt 3 pour l'utilisation d'API externe (Universalis).",
+        demoUrl: "https://ff14market-overview.netlify.app/about",
+        image: "ffxiv-market-helper-logo.webp",
+    }
+]
+
+export default projects
